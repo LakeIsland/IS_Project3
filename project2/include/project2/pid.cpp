422d761a993df57a4a527731e1732df2eda9a39a
LakeIsland <cjdeka3123@naver.com>
2017-12-05 11:12:33 +0900

    171205

--- project2/include/project2/pid.cpp
+++ project2/include/project2/pid.cpp
@@ -5,7 +5,7 @@
 #define THRESHOLD_R 0.2
 #define THRESHOLD_MAX_R 0.6
 
-#define MAX_ROTATE 0.4
+#define MAX_ROTATE 1
 
 PID::PID(){
 
@@ -18,7 +18,7 @@ PID::PID(){
     error = 0;
     error_sum = 0;
     error_diff = 0;
-    Kp = 0.9;
+    Kp = 1;
     Ki = 0;
     Kd = 0; 
 }
@@ -69,9 +69,9 @@ float PID::get_control(point car_pose, traj prev_goal, traj cur_goal) {
 	error = clampToPi(error);
 	
 	double prop_term = (this->Kp) * (this -> error);
-	double intg_term = (this->Ki) * (this -> error_sum) * dt;
+	double intg_term = (this->Ki) * (this -> error_sum);
 	//double diff_term = (this->Kd) * (error - (this->error)) / dt;
-	double diff_term = (this->Kd) * (this->error_diff) / dt;
+	double diff_term = (this->Kd) * (this->error_diff);
 	
 	double result = prop_term + intg_term + diff_term;
 
